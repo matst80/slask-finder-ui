5720ce18498fd501f921ea0400236aa3658c76ad
matst80 <mats.tornberg@gmail.com>
2024-08-14 19:52:24 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+      '/api':{
+        target:'https://slask-finder.tornberg.me',
+        changeOrigin:true,
+      },
+      '/admin':{
+        target:'https://slask-finder.tornberg.me',
+        changeOrigin:true,
+      }
+    }
+  },
   plugins: [react()],
 })
