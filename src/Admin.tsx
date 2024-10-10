766e4728baca4dc1749ee0f03555b83ab72c24bd
matst80 <mats.tornberg@gmail.com>
2024-10-10 22:19:57 +0200

    admin ui

--- src/Admin.tsx
+++ src/Admin.tsx
@@ -1,3 +1,27 @@
+import { AutoSuggest } from "./components/AutoSuggest";
+import { CurrentFilters } from "./components/CurrentFilters";
+import { Facets } from "./components/Facets";
+import { Paging } from "./components/Paging";
+import { ResultHeader } from "./components/ResultHeader";
+import { TableSearchResultList } from "./components/SearchResultList";
+
 export const Admin = () => {
-  return <div>Admin</div>;
+  return (
+    <div className="px-10 py-2 bg-gray-100">
+      <div className="flex gap-2 items-center mb-2 py-2 rounded-b-md">
+        <AutoSuggest />
+        {/* <MiniCart /> */}
+      </div>
+      <div className="flex flex-col md:flex-row gap-8">
+        <Facets />
+
+        <main className="flex-1">
+          <CurrentFilters />
+          <ResultHeader />
+          <TableSearchResultList />
+          <Paging />
+        </main>
+      </div>
+    </div>
+  );
 };
--- src/components/SearchResultList.tsx
+++ src/components/SearchResultList.tsx
@@ -2,10 +2,20 @@ import { useEffect, useRef, useState } from "react";
 import { useCategories } from "../categoryHooks";
 import { Category } from "../types";
 import { ResultItem } from "./ResultItem";
-import { SquareMinus, SquarePlus } from "lucide-react";
+import { Edit, SquareMinus, SquarePlus, Trash } from "lucide-react";
 import { useFilters, useHashQuery, useHashResultItems } from "../searchHooks";
 import { Impression, trackImpression } from "../beacons";
-import { ButtonLink } from "./ui/button";
+import { Button, ButtonLink } from "./ui/button";
+import { Checkbox } from "./ui/checkbox";
+import {
+  Table,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell,
+} from "./ui/table";
+import { Link } from "react-router-dom";
 
 const textSize = (level: number) => {
   switch (level) {
@@ -159,6 +169,61 @@ export const SearchResultList = () => {
       {results?.map((item, idx) => (
         <ResultItem key={item.id} {...item} position={start + idx} />
       ))}
+    </div>
+  );
+};
+
+export const TableSearchResultList = () => {
+  const { data: results, isLoading: loadingItems } = useHashResultItems();
+  const {
+    query: { query },
+  } = useHashQuery();
+
+  //const start = (page ?? 0) * (pageSize ?? 40);
+  if (loadingItems && (!results || !results.length)) {
+    return <div>Loading...</div>;
+  }
+
+  if (!results || (!results.length && (query == null || query.length < 1))) {
+    return <div>No results</div>;
+  }
+  return (
+    <div className="bg-white shadow-md rounded-lg overflow-hidden mb-6">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[50px]">Select</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Category</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead>Stock</TableHead>
+            <TableHead className="text-right">Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {results.map((product) => (
+            <TableRow key={product.id}>
+              <TableCell>
+                <Checkbox name={product.id} />
+              </TableCell>
+              <TableCell className="font-bold">{product.title}</TableCell>
+              <TableCell>{product.values["10"]}</TableCell>
+              <TableCell>{product.values["4"].toFixed(2)}</TableCell>
+              <TableCell>{product.stockLevel ?? "0"}</TableCell>
+              <TableCell className="justify-end flex gap-2">
+                <Button variant="ghost" size="icon">
+                  <Link to={`/product/${product.id}`}>
+                    <Edit className="h-4 w-4" />
+                  </Link>
+                </Button>
+                <Button variant="ghost" size="icon">
+                  <Trash className="h-4 w-4" />
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </div>
   );
 };
