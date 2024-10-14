c67d2fb3677eb87ea8c86ba6acc014b3502b818f
matst80 <mats.tornberg@gmail.com>
2024-10-14 10:36:37 +0200

    add save button

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -9,6 +9,8 @@ import {
   TableSearchResultList,
 } from "../components/SearchResultList";
 import { useKeyboardAdminToggle } from "../appState";
+import { useState } from "react";
+import { LoaderCircle } from "lucide-react";
 
 const AdminNavBar = () => {
   return (
@@ -44,6 +46,29 @@ const AdminNavBar = () => {
   );
 };
 
+export const SaveButton = () => {
+  const [loading, setLoading] = useState(false);
+  const save = () => {
+    setLoading(true);
+    fetch("/admin/save", { method: "POST" }).finally(() => setLoading(false));
+  };
+  return (
+    <div className="fixed bottom-0 right-0 p-4">
+      <button
+        disabled={loading}
+        className="bg-blue-500 text-white px-4 py-2 rounded-md"
+        onClick={save}
+      >
+        {loading ? (
+          <LoaderCircle className="size-4 animate-spin inline-block" />
+        ) : (
+          <span> Save changes </span>
+        )}
+      </button>
+    </div>
+  );
+};
+
 export const Admin = () => {
   useKeyboardAdminToggle();
   return (
@@ -51,6 +76,7 @@ export const Admin = () => {
       <AdminNavBar />
       <div className="px-10 py-2 bg-gray-100 min-h-screen">
         <Outlet />
+        <SaveButton />
       </div>
     </>
   );
