5617b2c3f57b69ed07004996db1688c342409e90
matst80 <mats.tornberg@gmail.com>
2025-04-13 11:15:38 +0200

    cleanup

--- src/components/ResultHeader.tsx
+++ src/components/ResultHeader.tsx
@@ -1,66 +1,64 @@
-import { useMemo, useState } from "react";
-import { getItemIds, updateCategories } from "../lib/datalayer/api";
-import { useAdmin } from "../hooks/appState";
+import { useMemo } from "react";
 
 import { Sorting } from "./Sorting";
 import { SelectedStore } from "./StoreSelector";
-import { X } from "lucide-react";
+
 import { useQuery } from "../lib/hooks/QueryProvider";
 import { FilterQuery } from "./FilterQuery";
 import { facetQueryToHash } from "../hooks/searchHooks";
 import { Button } from "./ui/button";
 
-const EditCategories = ({ onClose }: { onClose: () => void }) => {
-  const { facets, query } = useQuery();
+// const EditCategories = ({ onClose }: { onClose: () => void }) => {
+//   const { facets, query } = useQuery();
 
-  const updateItemCategories = (updates: { id: number; value: string }[]) => {
-    return getItemIds(query).then((ids) => {
-      //console.log(ids);
-      updateCategories(ids, updates);
-    });
-  };
+//   const updateItemCategories = (updates: { id: number; value: string }[]) => {
+//     return getItemIds(query).then((ids) => {
+//       //console.log(ids);
+//       updateCategories(ids, updates);
+//     });
+//   };
 
-  return (
-    <form
-      className="absolute top-0 right-0 bg-white p-4 shadow-lg z-10"
-      onSubmit={(e) => {
-        e.preventDefault();
-        const formData = new FormData(e.currentTarget);
+//   return (
+//     <form
+//       className="absolute top-0 right-0 bg-white p-4 shadow-lg z-10"
+//       onSubmit={(e) => {
+//         e.preventDefault();
+//         const formData = new FormData(e.currentTarget);
 
-        const categories = Array.from(formData.entries()).map(
-          ([id, value]) => ({
-            id: Number(id),
-            value: String(value),
-          })
-        );
-        //console.log(categories);
-        updateItemCategories(categories);
-      }}
-    >
-      <button onClick={onClose} type="button">
-        <X size={30} />
-      </button>
-      <ul className="flex flex-col gap-2">
-        {facets
-          ?.filter((d) => d.valueType === "virtual")
-          .map((category) => (
-            <li key={category.id} className="flex">
-              <label className="flex gap-4 items-center justify-between">
-                <span className="flex-1">{category.name}</span>
-                <input
-                  type="text"
-                  name={`${category.id}`}
-                  defaultValue=""
-                  className="border border-gray-400 rounded-md"
-                />
-              </label>
-            </li>
-          ))}
-      </ul>
-      <button type="submit">Save</button>
-    </form>
-  );
-};
+//         const categories = Array.from(formData.entries()).map(
+//           ([id, value]) => ({
+//             id: Number(id),
+//             value: String(value),
+//           })
+//         );
+//         //console.log(categories);
+//         updateItemCategories(categories);
+//       }}
+//     >
+//       <button onClick={onClose} type="button">
+//         <X size={30} />
+//       </button>
+//       <ul className="flex flex-col gap-2">
+//         {facets
+//           ?.filter((d) => d.valueType === "virtual")
+//           .map((category) => (
+//             <li key={category.id} className="flex">
+//               <label className="flex gap-4 items-center justify-between">
+//                 <span className="flex-1">{category.name}</span>
+//                 <input
+//                   type="text"
+//                   name={`${category.id}`}
+//                   defaultValue=""
+//                   className="border border-gray-400 rounded-md"
+//                 />
+//               </label>
+//             </li>
+//           ))}
+//       </ul>
+//       <button type="submit">Save</button>
+//     </form>
+//   );
+// };
 
 export const ResultHeader = () => {
   const { totalHits, queryHistory, query, setQuery } = useQuery();
