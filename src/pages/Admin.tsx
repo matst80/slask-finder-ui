e1047f38760e744baaba24c5ac072b735849e51a
matst80 <mats.tornberg@gmail.com>
2025-04-14 16:59:00 +0200

    remove strange save button

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,31 +1,29 @@
 import { Outlet } from "react-router-dom";
 import { useKeyboardAdminToggle } from "../hooks/appState";
-import { useState } from "react";
-import { LoaderCircle } from "lucide-react";
 import { AdminNavBar } from "./admin/AdminNavBar";
 
-export const SaveButton = () => {
-  const [loading, setLoading] = useState(false);
-  const save = () => {
-    setLoading(true);
-    fetch("/admin/save", { method: "POST" }).finally(() => setLoading(false));
-  };
-  return (
-    <div className="fixed bottom-0 right-0 p-4">
-      <button
-        disabled={loading}
-        className="bg-blue-500 text-white px-4 py-2 rounded-md"
-        onClick={save}
-      >
-        {loading ? (
-          <LoaderCircle className="size-4 animate-spin inline-block" />
-        ) : (
-          <span> Save changes </span>
-        )}
-      </button>
-    </div>
-  );
-};
+// export const SaveButton = () => {
+//   const [loading, setLoading] = useState(false);
+//   const save = () => {
+//     setLoading(true);
+//     fetch("/admin/save", { method: "POST" }).finally(() => setLoading(false));
+//   };
+//   return (
+//     <div className="fixed bottom-0 right-0 p-4">
+//       <button
+//         disabled={loading}
+//         className="bg-blue-500 text-white px-4 py-2 rounded-md"
+//         onClick={save}
+//       >
+//         {loading ? (
+//           <LoaderCircle className="size-4 animate-spin inline-block" />
+//         ) : (
+//           <span> Save changes </span>
+//         )}
+//       </button>
+//     </div>
+//   );
+// };
 
 export const Admin = () => {
   useKeyboardAdminToggle();
@@ -34,7 +32,7 @@ export const Admin = () => {
       <AdminNavBar />
       <div className="px-10 py-2 bg-gray-100 min-h-screen">
         <Outlet />
-        <SaveButton />
+        {/* <SaveButton /> */}
       </div>
     </>
   );
