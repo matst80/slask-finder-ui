6b9d4ecd4ccf2d91397fee3df7413d002c8e534c
matst80 <mats.tornberg@gmail.com>
2025-04-27 15:43:33 +0200

    lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,7 +11,7 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
   rules: {
-    "@typescript-eslint/no-unused-vars": "error",
+    "unused-imports/no-unused-imports": "error",
     "react-refresh/only-export-components": [
       "error",
       { allowConstantExport: true },
