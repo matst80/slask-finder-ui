5b293c183a157ebe67a085957943ffa33540c16d
matst80 <mats.tornberg@gmail.com>
2025-04-27 12:19:48 +0200

    fix

--- src/components/Facets.tsx
+++ src/components/Facets.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { isNumberFacet, KeyFacet } from "../lib/types";
 import { ChevronUp, LoaderCircle } from "lucide-react";
 
