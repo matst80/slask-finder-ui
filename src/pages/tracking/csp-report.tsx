6c8da164055b4b36f656080fc821ac48979cf36c
matst80 <mats.tornberg@gmail.com>
2025-04-28 12:10:21 +0200

    jsonview

--- src/pages/tracking/csp-report.tsx
+++ src/pages/tracking/csp-report.tsx
@@ -80,7 +80,7 @@ const JsonView = ({ data }: { data: unknown }) => {
   const copy = useClipboard();
   return (
     <pre
-      className="bg-black text-white p-4 text-sm overflow-x-auto rounded-md"
+      className="bg-black text-white p-4 text-sm overflow-x-hidden rounded-md"
       onClick={() => copy(JSON.stringify(data, null, 2))}
     >
       <code>{JSON.stringify(data, null, 2)}</code>
