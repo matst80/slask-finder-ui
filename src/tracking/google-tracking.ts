65e559d922bb2cc1915807235b07d1b0fc397326
matst80 <mats.tornberg@gmail.com>
2025-05-02 12:29:18 +0200

    impressions

--- src/tracking/google-tracking.ts
+++ src/tracking/google-tracking.ts
@@ -4,6 +4,7 @@ import {
 } from "../lib/hooks/TrackingContext";
 
 type WindowWithDataLayer = Window & {
+  gtag?: (event: "event" | "config", ...args: any[]) => void;
   dataLayer?: unknown[];
 };
 
@@ -15,9 +16,34 @@ export const googleTracker = (
     context: context || {},
     track: (event, context) => {
       const w = globalThis?.window as WindowWithDataLayer;
-      console.log("google tracker", { event, context, dataLayer: w.dataLayer });
-      if (w?.dataLayer) {
-        w?.dataLayer.push(event);
+      console.log("google tracker", { event, context, dataLayer: w.gtag });
+      if (w?.gtag) {
+        switch (event.type) {
+          case "impressions":
+            w.gtag("event", "view_item_list", {
+              ...context,
+              items: event.items.map((item) => ({
+                item_name: item.name,
+                item_id: item.id,
+                item_brand: item.brand,
+                price: item.price,
+                index: item.position,
+                currency: "SEK",
+                ...(item.category
+                  ? Object.fromEntries(
+                      item.category.map(
+                        (value, level) =>
+                          [`item_category_${level + 1}`, value] as [
+                            string,
+                            string
+                          ]
+                      )
+                    )
+                  : {}),
+              })),
+            });
+            break;
+        }
       }
     },
   };
