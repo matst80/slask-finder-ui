72461303a2ba130d842933410c79796b556de6c5
matst80 <mats.tornberg@gmail.com>
2025-05-02 12:34:12 +0200

    better tracking

--- src/tracking/google-tracking.ts
+++ src/tracking/google-tracking.ts
@@ -8,39 +8,45 @@ type WindowWithDataLayer = Window & {
   dataLayer?: unknown[];
 };
 
+const getCategories = (category: string[] | undefined) =>
+  category
+    ? Object.fromEntries(
+        category.map(
+          (value, level) =>
+            [
+              level === 0 ? "item_category" : `item_category_${level + 1}`,
+              value,
+            ] as [string, string]
+        )
+      )
+    : {};
+
 export const googleTracker = (
   context?: GoogleTrackingContext
 ): GoogleTracker => {
   return {
     type: "google",
     context: context || {},
-    track: (event, context) => {
+    track: (event, { list_id, list_name }) => {
       const w = globalThis?.window as WindowWithDataLayer;
       console.log("google tracker", { event, context, dataLayer: w.gtag });
       if (w?.gtag) {
         switch (event.type) {
           case "impressions":
             w.gtag("event", "view_item_list", {
-              ...context,
-              items: event.items.map((item) => ({
-                item_name: item.name,
-                item_id: item.id,
-                item_brand: item.brand,
-                price: item.price,
-                index: item.position,
-                currency: "SEK",
-                ...(item.category
-                  ? Object.fromEntries(
-                      item.category.map(
-                        (value, level) =>
-                          [`item_category_${level + 1}`, value] as [
-                            string,
-                            string
-                          ]
-                      )
-                    )
-                  : {}),
-              })),
+              list_name,
+              list_id,
+              items: event.items.map(
+                ({ name, id, brand, position, category, ...data }) => ({
+                  ...data,
+                  item_name: name,
+                  item_id: String(id),
+                  item_brand: brand,
+                  index: position,
+                  currency: "SEK",
+                  ...getCategories(category),
+                })
+              ),
             });
             break;
         }
