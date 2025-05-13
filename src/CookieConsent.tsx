2042a6021bec6e3dc44db0eefb33a8c897b49200
matst80 <mats.tornberg@gmail.com>
2025-05-13 16:36:10 +0200

    update consent

--- src/CookieConsent.tsx
+++ src/CookieConsent.tsx
@@ -48,7 +48,7 @@ export const useCookieAcceptance = () => {
 
 export const CookieConsent = () => {
   const { accepted, updateAccept } = useCookieAcceptance();
-
+  const [detailsOpen, setDetailsOpen] = useState(false);
   const [open, setOpen] = useState<boolean>(accepted == null);
   const handleAccept = (value: CookieAcceptanceLevel) => (e: MouseEvent) => {
     e.preventDefault();
@@ -71,21 +71,37 @@ export const CookieConsent = () => {
   return (
     <Dialog open={open} setOpen={setOpen} attached="bottom">
       <div className="flex items-center justify-between p-6 bg-white border-t border-gray-200 shadow-lg">
-        <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-          <div className="flex flex-col">
+        <div className="container mx-auto flex flex-col md:flex-row gap-2 justify-between items-center">
+          <div className="flex flex-col gap-2">
             <p>
-              This site uses cookies, "cartid" if you use the cart, "sid" for
-              session tracking, "locale" for language selection and "sfadmin" if
-              you login and "ca" for consent.
+              This site uses cookies, basic function cookies and also optional
+              for improving search results for you and others,{" "}
+              <a
+                href="#details"
+                onClick={() => setDetailsOpen((p) => !p)}
+                className="underline hover:no-underline"
+              >
+                details
+              </a>
+              .
             </p>
+            {detailsOpen && (
+              <p className="my-2 text-gray-700 animate-pop-slow">
+                "cartid" if you use the cart, "sid" for session tracking,
+                "locale" for language selection and "sfadmin" if you login and
+                "ca" for consent.
+              </p>
+            )}
             <p className="text-sm text-gray-500">
               By clicking "Accept" you consent to the use of cookies. You can
               also choose to reject cookies.
             </p>
           </div>
-          <form className="flex items-center gap-2" method="dialog">
+          <form className="flex gap-2 items-center" method="dialog">
             <Button
               variant="default"
+              type="submit"
+              autoFocus
               onClick={handleAccept("all")}
               tabIndex={1}
             >
