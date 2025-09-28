5f1d600cdf31e632935e7c67cf30ba4a8d511820
Mats Törnberg <mats@tornberg.me>
2025-09-28 08:59:04 +0200

    update

--- src/components/toImpression.tsx
+++ src/components/toImpression.tsx
@@ -11,7 +11,7 @@ export const toEcomTrackingEvent = (
   item_category2: item.values?.[11],
   item_category3: item.values?.[12],
   item_category4: item.values?.[13],
-  item_category5: item.values?.[14],
+  //item_category5: item.values?.[14],
   item_brand: item.values?.["2"],
   price: item.values?.["4"],
   //stock: item.stockLevel!=null,
