14e5f4c7993a8ee608431189dfad5d0fd4c3c465
matst80 <mats.tornberg@gmail.com>
2025-10-12 14:16:51 +0200

    update cart hooks

--- src/hooks/cartHooks.ts
+++ src/hooks/cartHooks.ts
@@ -1,78 +1,84 @@
-import useSWR from 'swr'
-import { clearCart } from '../lib/datalayer/api'
+import useSWR from "swr";
+import { clearCart } from "../lib/datalayer/api";
 import {
   addToCart,
   changeQuantity,
   getCart,
   removeFromCart,
-} from '../lib/datalayer/cart-api'
-import { useFetchMutation } from '../utils'
-import { trackCart } from '../lib/datalayer/beacons'
-import { BaseEcomEvent, Item } from '../lib/types'
-import { useNotifications } from '../components/ui-notifications/useNotifications'
-import { toEcomTrackingEvent } from '../components/toImpression'
-import { useCookieAcceptance } from '../CookieConsent'
+} from "../lib/datalayer/cart-api";
+import { isDefined, useFetchMutation } from "../utils";
+import { trackCart } from "../lib/datalayer/beacons";
+import { BaseEcomEvent, Item } from "../lib/types";
+import { useNotifications } from "../components/ui-notifications/useNotifications";
+import { toEcomTrackingEvent } from "../components/toImpression";
+import { useCookieAcceptance } from "../CookieConsent";
 
-const cartKey = '/cart'
+const cartKey = "/cart";
 
 export const useCart = () => {
   return useSWR(cartKey, getCart, {
     keepPreviousData: true,
-    errorRetryInterval: 50000,
-  })
-}
+    revalidateOnFocus: true,
+    errorRetryInterval: 5000,
+  });
+};
 
 export const useResetCart = () => {
-  return useFetchMutation(cartKey, () => clearCart().then(() => getCart()))
-}
+  return useFetchMutation(cartKey, () => clearCart().then(() => getCart()));
+};
 
 export const useAddMultipleToCart = () => {
-  const { showNotification } = useNotifications()
+  const { showNotification } = useNotifications();
   return useFetchMutation(cartKey, (items: (Item & { quantity?: number })[]) =>
     Promise.all(
-      items.map((item, idx) => {
-        return addToCart({ sku: item.sku, quantity: item.quantity ?? 1 })
-          .then(() => {
+      items.map((item, idx) =>
+        addToCart({ sku: item.sku, quantity: item.quantity ?? 1 })
+          .then((cart) => {
             showNotification({
-              title: 'Added to cart',
+              title: "Added to cart",
               message: `${item.title} has been added to your cart.`,
-              variant: 'success',
-            })
+              variant: "success",
+            });
             trackCart({
               ...toEcomTrackingEvent(item, idx),
               quantity: item.quantity ?? 1,
-              type: 'add',
-            })
+              type: "add",
+            });
+            return cart;
           })
           .catch(() => {
             showNotification({
-              title: 'Error',
+              title: "Error",
               message: `Failed to add ${item.title} to your cart.`,
-              variant: 'error',
-            })
-          })
-      }),
-    ),
-  )
-}
+              variant: "error",
+            });
+            return null;
+          }),
+      ),
+    ).then((r) => {
+      const carts = r.filter(isDefined);
+      return carts[carts.length - 1];
+    }),
+  );
+};
 
 export const useAddToCart = () => {
-  const { accepted } = useCookieAcceptance()
-  const { showNotification } = useNotifications()
-  const { trigger, ...rest } = useFetchMutation(cartKey, addToCart)
+  const { accepted } = useCookieAcceptance();
+  const { showNotification } = useNotifications();
+  const { trigger, ...rest } = useFetchMutation(cartKey, addToCart);
   return {
     ...rest,
     trigger: async (
       item: { sku: string; quantity: number; storeId?: string },
       trackingItem: BaseEcomEvent,
     ) => {
-      if (accepted === 'none' || accepted === null) {
+      if (accepted === "none" || accepted === null) {
         showNotification({
-          title: 'Error',
+          title: "Error",
           message: `No use in adding to cart if you don't accept essential cookies.`,
-          variant: 'error',
-        })
-        return
+          variant: "error",
+        });
+        return;
       }
       return trigger(item)
         .then((data) => {
@@ -85,46 +91,46 @@ export const useAddToCart = () => {
             trackCart({
               ...trackingItem,
               quantity: item.quantity,
-              type: 'add',
-            })
+              type: "add",
+            });
           }
-          return data
+          return data;
         })
         .catch(() => {
           showNotification({
-            title: 'Error',
+            title: "Error",
             message: `Failed to add item to your cart.`,
-            variant: 'error',
-          })
-        })
+            variant: "error",
+          });
+        });
     },
-  }
-}
+  };
+};
 
 export const useChangeQuantity = () => {
-  const { trigger, ...rest } = useFetchMutation(cartKey, changeQuantity)
+  const { trigger, ...rest } = useFetchMutation(cartKey, changeQuantity);
   return {
     ...rest,
     trigger: async (id: number, quantity: number, item: BaseEcomEvent) => {
-      const data = await trigger({ id, quantity })
+      const data = await trigger({ id, quantity });
       if (data) {
-        trackCart({ ...item, quantity, type: 'quantity' })
+        trackCart({ ...item, quantity, type: "quantity" });
       }
-      return data
+      return data;
     },
-  }
-}
+  };
+};
 
 export const useRemoveFromCart = () => {
-  const { trigger, ...rest } = useFetchMutation(cartKey, removeFromCart)
+  const { trigger, ...rest } = useFetchMutation(cartKey, removeFromCart);
   return {
     ...rest,
     trigger: async (id: number, item: BaseEcomEvent) => {
-      const data = await trigger({ id })
+      const data = await trigger({ id });
       if (data) {
-        trackCart({ ...item, quantity: 0, type: 'remove' })
+        trackCart({ ...item, quantity: 0, type: "remove" });
       }
-      return data
+      return data;
     },
-  }
-}
+  };
+};
