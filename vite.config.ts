ad401de38ea9d214ee7daf25cbd6f2c4e04a1361
matst80 <mats.tornberg@gmail.com>
2025-10-13 22:01:26 +0200

    d

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target, //: localTarget,
+        target: localTarget,
         changeOrigin: true,
       },
       "/cart": {
