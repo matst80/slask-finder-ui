690f55433d99492e1917ba020fc35dc21c1d8c77
matst80 <mats.tornberg@gmail.com>
2025-11-29 14:44:55 +0100

    update

--- src/lib/adyen-config.ts
+++ src/lib/adyen-config.ts
@@ -3,15 +3,15 @@ import { CoreConfiguration } from '@adyen/adyen-web'
 export const adyenConfig: Partial<CoreConfiguration> = {
   clientKey: 'test_M6LOH6NGD5DA5ATLXYXD7M432IME5URR',
   environment: 'test',
-  locale: 'en_US',
-  countryCode: 'NL',
+  locale: 'sv_SE',
+  countryCode: 'SE',
   showPayButton: true,
 }
 export const paymentMethodsConfiguration = {
   card: {
     showBrandIcon: true,
-    hasHolderName: true,
-    holderNameRequired: true,
+    hasHolderName: false,
+    holderNameRequired: false,
     placeholders: {
       cardNumber: '1234 5678 9012 3456',
       expiryDate: 'MM/YY',
